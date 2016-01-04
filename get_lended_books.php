6436700cdf76df3cabdabc2b984fc640b1f3a787
Jonatan Norbye <jonatan@norbye.com>
2016-01-04 20:15:26 +0100

    Updated formatting of output

--- get_lended_books.php
+++ get_lended_books.php
@@ -1,16 +1,16 @@
-﻿<?php
+<?php
 //kobler til database
 require('../koble_til_database.php');
 session_start();
 
 $nonexistant_user = 'User does not exist.';
 
-$username = $_GET['username'];
+$username = $_POST['username'];
 
 $get_user = "SELECT userID FROM lib_User WHERE username='" . $username . "'";
 $get_user_info_result = $conn->query($get_user);
 
-$res = array();
+$res = array('error' => "");
 
 if ($get_user_info_result->num_rows > 0) {
     while($user = $get_user_info_result->fetch_assoc()){
@@ -18,14 +18,17 @@ if ($get_user_info_result->num_rows > 0) {
         $get_books_qry = $conn->query($get_books);
         if($get_books_qry->num_rows > 0){
             while($book = $get_books_qry->fetch_assoc()){
-                $res[$book['bookID']]['outData'] = $book['outDate'];
-                $res[$book['bookID']]['inData'] = $book['inDate'];
+                
                 $get_book_details = "SELECT * FROM lib_Book WHERE bookID='".$book['bookID']."'";
                 $get_book_details_qry = $conn->query($get_book_details);
                 if($get_book_details_qry->num_rows > 0){
                     if($book_details = $get_book_details_qry->fetch_assoc()){
-                        $res[$book['bookID']]['RFID'] = $book_details['RFID'];
-                        $res[$book['bookID']]['ISBN'] = $book_details['ISBN'];
+                        $res['books'][] = array(
+                            'outDate' => $book['outDate'],
+                            'inDate' => $book['inDate'],
+                            'RFID' => $book_details['RFID'],
+                            'ISBN' => $book_details['ISBN']
+                        );
                     }
                 }
             }
