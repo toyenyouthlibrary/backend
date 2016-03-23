d391fb7fcc0aa5b56f2daede50f87a3494b2faab
jonatannorbye <jonatan@norbye.com>
2016-03-23 23:23:14 +0100

    Fixed feedback.php

--- feedback.php
+++ feedback.php
@@ -11,21 +11,50 @@ $error = array(
     'unaccepted_int' => 'Stjernevurderinger m&aring; v&aelig;re mellom 1 og 10.',
     'nonexistant_user' => 'Den etterspurte brukeren finnes ikke.',
     'nonexistant_book' => 'Den etterspurte boken finnes ikke.',
-    'failed_save' => 'Klarte ikke &aring; lagre feedback.'
+    'failed_save' => 'Klarte ikke &aring; lagre feedback.',
+    'missing_rfid' => 'Man m&aring; sende 2 RFIDer.',
+    'wrong_rfid_types' => 'Man m&aring; sende med 1 bok og 1 bruker.'
 );
 
 
-if(!isset($_POST['user_rfid']) || !isset($_POST['book_rfid']) || !isset($_POST['type']) || !isset($_POST['value'])){
+if(!isset($_POST['rfid']) || !isset($_POST['type']) || !isset($_POST['value'])){
     j_die($error['missing_data']);
 }
-$user_rfid = $_POST['user_rfid'];
-$book_rfid = $_POST['book_rfid'];
+$rfid = $_POST['rfid'];
 $type = $_POST['type'];
 $value = $_POST['value'];
 
 //Verify the type of feedback
 if(!($type == "comment" || $type == "star")){
     j_die($error['unknown_feedback']);
+}
+
+//Verify the RFIDs
+$rfid = trim($rfid, ";");
+$rfids = explode(";", $rfid);
+if(count($rfids) == 2){
+    require 'rfid.class.php';
+    $rfid = new RFID();
+    $user = -1;
+    $book = -1;
+    for($i = 0; $i < count($rfids); $i++){
+        $res = $rfid->type($rfids[$i]);
+        if($res[0] == 'user'){
+            if($user === -1){
+                $user = $rfids[$i];
+            }else{
+                j_die($error['wrong_rfid_types']);
+            }
+        }else if($res[0] == 'book'){
+            if($book === -1){
+                $book = $rfids[$i];
+            }else{
+                j_die($error['wrong_rfid_types']);
+            }
+        }
+    }
+}else{
+    j_die($error['missing_rfid']);
 }
 
 //Verify the type of value
@@ -43,37 +72,10 @@ if($type == "comment"){
     }
 }
 
-//Verify that the user exists
-$get_user = "SELECT userID FROM lib_User WHERE rfid = '" . $user_rfid . "'";
-$get_user_qry = $conn->query($get_user);
-
-if($get_user_qry->num_rows > 0){
-    if($user = $get_user_qry->fetch_assoc()){
-        
-    }else{
-        j_die($error['nonexistant_user']);
-    }
-}else{
-    j_die($error['nonexistant_user']);
-}
-
-//Verify that the book exists
-$get_book = "SELECT bookID FROM lib_RFID WHERE RFID = '" . $book_rfid . "' AND bookID != 0";
-$get_book_qry = $conn->query($get_book);
-if($get_book_qry->num_rows > 0){
-    if($book = $get_book_qry->fetch_assoc()){
-        
-    }else{
-        j_die($error['nonexistant_book']);
-    }
-}else{
-    j_die($error['nonexistant_book']);
-}
-
 //Save the comment
 $date= (new DateTime())->format('Y-m-d H:i:s');
 
-$save_feedback = "INSERT INTO lib_Feedback (user_rfid, book_rfid, type, value, timestamp) VALUES ('".$user_rfid."', '".$book_rfid."', '".$type."', '".$value."', '".$date."')";
+$save_feedback = "INSERT INTO lib_Feedback (user_rfid, book_rfid, type, value, timestamp) VALUES ('".$user."', '".$book."', '".$type."', '".$value."', '".$date."')";
 $save_feedback_qry = $conn->query($save_feedback);
 if($save_feedback_qry === TRUE){
     //Success
