5c319ad0f36b8f5bdc8c7ce491d932fe1ccfb82a
jonatannorbye <jonatan@norbye.com>
2016-03-30 00:52:31 +0200

    Enabled error reporting

--- frontend.php
+++ frontend.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL);
 define("ROOT", getcwd()."/api/");
 define("DEPENDENCIES_LOC", "frontend_resources/");
 define("DEPENDENCIES_URL", ROOT."../frontend_resources/");
@@ -7,7 +8,6 @@ if(isset($_GET['index'])){
 }
 require '../koble_til_database.php';
 session_start();
-
 //Initialize result array with the part that will always be the same
 $res = array('error' => '');
 
@@ -28,6 +28,6 @@ if(isset($_GET['index'])){
         require DEPENDENCIES_URL.'footer.php';
     }
 }else{
-    header("Location: /frontent/");
+    header("Location: /frontend/");
 }
 ?>
